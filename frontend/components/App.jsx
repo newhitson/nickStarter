570c909c7d3bed9417d746d84af30ea095ad6229
Nicholas Whitson <newhitson@gmail.com>
2017-07-18 14:20:58 -0700

    loggin/sign up buttons kind of work

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import {
+  Route,
+  Redirect,
+  Switch,
+  Link,
+  HashRouter
+} from 'react-router-dom';
+
+
+
 import GreetingContainer from './greeting/greeting_container';
+import SessionFormContainer from './session_form/session_form_container';
+
 
 const App = () => (
   <div>
-    <h1>nickStarter</h1>
-    <GreetingContainer />
+    <header>
+      <h1>nickStarter</h1>
+      <GreetingContainer />
+    </header>
+    <switch>
+      <Route path="/login" component={SessionFormContainer} />
+      <Route path="/signup" component={SessionFormContainer} />
+    </switch>
   </div>
 );
 
