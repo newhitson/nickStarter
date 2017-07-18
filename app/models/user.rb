c08a131b6d89c8414e8bd43390e28392b9717522
Nicholas Whitson <newhitson@gmail.com>
2017-07-17 23:46:58 -0700

    Add Auth models and controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,41 @@
 class User < ApplicationRecord
+validates :username, :password_digest, :session_token, presence: true
+validates :username, uniqueness: true
+validates :password, length: {minimum: 6}, allow_nil: :true
+
+after_initialize :ensure_session_token
+
+attr_reader :password
+
+def password= password
+  self.password_digest = BCrypt::Password.create(password)
+  @password = password
+end
+
+def self.find_by_credentials username, password
+  user = User.find_by(username: username)
+  return nil unless user
+  user.password_is?(password) ? user : nil
+end
+
+def password_is? password
+  BCrypt::Password.new(self.password_digest).is_password?(password)
+end
+
+def reset_session_token!
+  self.session_token = new_session_token
+  self.save
+  self.session_token
+end
+
+private
+
+def ensure_session_token
+  self.session_token ||= new_session_token
+end
+
+def new_session_token
+  SecureRandom.base64
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
 
 
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  get 'users/create'
+
   root "static_pages#root"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
