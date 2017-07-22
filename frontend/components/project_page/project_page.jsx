d67046b70a0976bd595eead3e75e57bcd20d2fbf
Nicholas Whitson <newhitson@gmail.com>
2017-07-22 12:56:59 -0700

    project_page_image_size

--- frontend/components/project_page/project_page.jsx
+++ frontend/components/project_page/project_page.jsx
@@ -38,17 +38,25 @@ class ProjectPage extends React.Component {
             </div>
           </header>
           <figure className="project_picture_conatiner">
-            <figure className="main_project_image">
-              <img className="main_project_image_image"
-                src="https://www.planwallpaper.com/static/images/desktop-year-of-the-tiger-images-wallpaper.jpg" />
+            <figure className="main_project_image_container">
+              <img className="main_project_image"
+                src="https://ksr-ugc.imgix.net/assets/016/537/903/bca7902e3232309d81454d5aab22eeee_original.jpg?crop=faces&w=1024&h=576&fit=crop&v=1495761944&auto=format&q=92&s=fafc3f77199e2058225691a7e04f7c2c"
+                />
             </figure>
             <figure className="project_funded_info">
               <p>{project.funding_goal}</p>
             </figure>
           </figure>
-          <figure className="project_rewards_container">
+          <figure className="project_about_container">
+            <figure className="project_page_description_container">
+              <figure className= "project_page_description_title">
+              </figure>
+              <figure className="project_page_description_container">
+              </figure>
+            </figure>
+            <figure className="project_page_rewards_container"></figure>
           </figure>
-          <h1>{project.title}</h1>
+
         </section>
         <br/>
         <br/>
