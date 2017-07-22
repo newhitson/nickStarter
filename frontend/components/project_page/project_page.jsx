9ec2a0b6929b097b6d7e3901c515e977d69348f2
Nicholas Whitson <newhitson@gmail.com>
2017-07-22 15:09:03 -0700

    project page, draft, except rewards and funded

--- frontend/components/project_page/project_page.jsx
+++ frontend/components/project_page/project_page.jsx
@@ -18,7 +18,7 @@ class ProjectPage extends React.Component {
       if (projects.project){
         const project = projects.project;
         return (
-      <div>
+      <div className="project_page_background">
         <br/>
         <br/>
         <br/>
@@ -26,14 +26,18 @@ class ProjectPage extends React.Component {
         <section className="project_page_container">
           <header className="project_header">
             <div className="project_header_creator_box">
-              <p>By: {project.creator}</p>
+              <img src="https://ksr-ugc.imgix.net/assets/006/077/755/4036c410db5007a610236fa27e5bd8b8_original.jpg?w=80&h=80&fit=crop&v=1471144727&auto=format&q=92&s=362e3a27b8725fc0b4c09ce817c5daa2" />
+              <div className="project_header_by_box">
+              <p className="by">By</p>
+              <p className="creator">{project.creator}</p>
+              </div>
             </div>
             <div className="project_header_project_desciption_box">
               <div className="project_header_project_title">
-                <p>{project.title}</p>
+                <p className="project_page_title">{project.title}</p>
               </div>
               <div className="project_header_project_tag_line">
-                <p>{project.tagline}</p>
+                <p className="project_page_tagline">{project.tagline}</p>
               </div>
             </div>
           </header>
@@ -50,11 +54,16 @@ class ProjectPage extends React.Component {
           <figure className="project_about_container">
             <figure className="project_page_description_container">
               <figure className= "project_page_description_title">
+                <p className="description_title">
+                  About this project</p>
               </figure>
-              <figure className="project_page_description_container">
+              <figure className="project_description_box">
+                <p className="project_description">{project.description}</p>
               </figure>
             </figure>
-            <figure className="project_page_rewards_container"></figure>
+            <figure className="project_page_rewards_container">
+              <p>rewards</p>
+            </figure>
           </figure>
 
         </section>
