e6bd354a106a29d742b6a8b4c40ccc3996e6f0d3
Nicholas Whitson <newhitson@gmail.com>
2017-07-26 10:59:06 -0700

    reward ajax and project action

---
+++ frontend/actions/rewards_actions.js
@@ -0,0 +1,30 @@
+import * as APIUtil from '../util/reward_api_util';
+
+export const RECEIVE_SINGLE_REWARD = 'RECEIVE_SINGLE_REWARD';
+export const RECEIVE_REWARD_ERRORS = 'RECEIVE_REWARD_ERRORS';
+export const RECEIVE_ALL_REWARDS = 'RECEIVE_ALL_REWARDS';
+
+export const receiveSingleReward = rewards => ({
+  type: RECEIVE_SINGLE_REWARD,
+  rewards
+});
+
+export const receiveRewardErrors = errors => {
+  return ({
+  type: RECEIVE_REWARD_ERRORS,
+  errors
+  });
+};
+
+export const receiveAllRewards = rewards => ({
+  type: RECEIVE_ALL_REWARDS,
+  rewards
+});
+
+
+export const createReward = reward => dispatch => (
+  APIUtil.createReward(reward).then(newReward => {
+    dispatch(receiveSingleReward(newReward));
+    return newReward;
+  }).fail(err => dispatch(receiveRewardErrors(err.responseJSON)))
+);
